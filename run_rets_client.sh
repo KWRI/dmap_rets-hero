c85c7e0b08bf0a0596a5639924dcdd3a530d2c02
yanirregev.kwri <yanir.regev2@kw.com>
2025-10-24 14:52:02 -0700

    Cleaned up dirctory

--- run_rets_client.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Activate the virtual environment
-source /home/yanirregev/git/dmap-tools/rets_python/venv_rets/bin/activate
-
-# Run the Python script
-exec python /home/yanirregev/git/dmap-tools/rets_python/refindly_rets_v2.py
